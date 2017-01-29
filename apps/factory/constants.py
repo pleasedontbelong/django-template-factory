ac5c8f4d456106d7eb0f77627aebf348e3a74bf0
Pablo Leano <pleasedontbelong@gmail.com>
2017-01-30 00:07:31 +0100

    Add extended choices to form and draft generate settings

--- apps/factory/constants.py
+++ apps/factory/constants.py
@@ -4,5 +4,13 @@ from extended_choices import Choices
 CORE_REPO = "git@github.com:pleasedontbelong/django-template.git"
 ADDONS = Choices(
     ('JINJA2', 'addon/jinja2/2_8', 'Jinja2 Templates'),
-    ('PIPELINE', 'addon/pipeline/1_6', 'Pipeline')
+    ('PIPELINE', 'addon/pipeline/1_6', 'Pipeline'),
+    ('EXTENDED_CHOICES', 'addon/extended_choices/1_1', 'Pipeline'),
+
 )
+
+FIELDS_MAP = {
+    "pipeline": ADDONS.PIPELINE,
+    "jinja2": ADDONS.JINJA2,
+    "extended_choices": ADDONS.EXTENDED_CHOICES,
+}
--- apps/website/views.py
+++ apps/website/views.py
@@ -5,3 +5,7 @@ from factory.forms import FactoryForm
 class HomeView(FormView):
     template_name = "home.jinja2"
     form_class = FactoryForm
+
+    def form_valid(self, form):
+        form.generate()
+        return super(HomeView, self).form_valid(form)
