15efcf81608fed150e62e8b7c62334a15c983cb7
Pablo Leano <pleasedontbelong@gmail.com>
2017-03-18 00:40:00 +0100

    Fix production environment

--- template_factory/settings/prod.py
+++ template_factory/settings/prod.py
@@ -1,11 +1,24 @@
 # https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 from .base import *  # NOQA
+from os import environ
+from os.path import join
 
 DEBUG = False
 TEMPLATE_DEBUG = False
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 
-# PARENT_HOST = 'mysite.com'
-# ALLOWED_HOSTS = ['.%s' % PARENT_HOST]
-# SESSION_COOKIE_DOMAIN = ALLOWED_HOSTS[0]
-# SESSION_COOKIE_NAME = 'mysite_sid'
+SECRET_KEY = environ['SECRET_KEY']
+
+PARENT_HOST = 'django-template-factory.herokuapp.com'
+ALLOWED_HOSTS = ['.%s' % PARENT_HOST]
+SESSION_COOKIE_DOMAIN = ALLOWED_HOSTS[0]
+SESSION_COOKIE_NAME = 'django-template-factory'
+
+MIDDLEWARE += [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+]
+
+STATIC_ROOT = join(BASE_DIR, "static")
+STATIC_URL = "/static/"
