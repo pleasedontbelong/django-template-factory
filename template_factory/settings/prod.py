129e19ba7ae034ef6b1244f3e348490b524fe6eb
Pablo Leano <pleasedontbelong@gmail.com>
2017-04-17 12:06:44 +0200

    Add logger for production

--- template_factory/settings/prod.py
+++ template_factory/settings/prod.py
@@ -26,3 +26,19 @@ STATIC_ROOT = join(BASE_DIR, "static")
 STATIC_URL = "/static/"
 
 FORCE_SSL = True
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+        },
+    },
+}
